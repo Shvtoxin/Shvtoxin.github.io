6af4480d1c9b89c8cf1c33482ae15d4d2c8bcdbf
Shvtoxin <95116781+Shvtoxin@users.noreply.github.com>
2024-01-14 11:31:00 +0300

    Update data_load_jquery.js

--- data_load_jquery.js
+++ data_load_jquery.js
@@ -2,51 +2,11 @@ ymaps.ready()
     .done(function (ym) {
         var myMap = new ym.Map('YMapsID', {
             center: [55.751574, 37.573856],
-            zoom: 10,
-            // Добавим панель маршрутизации.
-            controls: ['routePanelControl']
+            zoom: 10
         }, {
             searchControlProvider: 'yandex#search'
         });
 
-    var control = myMap.controls.get('routePanelControl');
-
-    // Зададим состояние панели для построения машрутов.
-    control.routePanel.state.set({
-        // Тип маршрутизации.
-        type: 'masstransit',
-        // Выключим возможность задавать пункт отправления в поле ввода.
-        fromEnabled: false,
-        // Адрес или координаты пункта отправления.
-        //from: 'Москва, Льва Толстого 16',
-        // Включим возможность задавать пункт назначения в поле ввода.
-        toEnabled: true
-        // Адрес или координаты пункта назначения.
-        //to: 'Петербург'
-    });
-
-    // Зададим опции панели для построения машрутов.
-    control.routePanel.options.set({
-        // Запрещаем показ кнопки, позволяющей менять местами начальную и конечную точки маршрута.
-        allowSwitch: false,
-        // Включим определение адреса по координатам клика.
-        // Адрес будет автоматически подставляться в поле ввода на панели, а также в подпись метки маршрута.
-        reverseGeocoding: true,
-        // Зададим виды маршрутизации, которые будут доступны пользователям для выбора.
-        types: { masstransit: true, pedestrian: true, taxi: true }
-    });
-
-    // Создаем кнопку, с помощью которой пользователи смогут менять местами начальную и конечную точки маршрута.
-    var switchPointsButton = new ymaps.control.Button({
-        data: {content: "Поменять местами", title: "Поменять точки местами"},
-        options: {selectOnClick: false, maxWidth: 160}
-    });
-    // Объявляем обработчик для кнопки.
-    switchPointsButton.events.add('click', function () {
-        // Меняет местами начальную и конечную точки маршрута.
-        control.routePanel.switchPoints();
-    });
-    myMap.controls.add(switchPointsButton);
         jQuery.getJSON('GeMap.json', function (json) {
             /** Сохраним ссылку на геообъекты на случай, если понадобится какая-либо постобработка.
              * @see https://api.yandex.ru/maps/doc/jsapi/2.1/ref/reference/GeoQueryResult.xml
